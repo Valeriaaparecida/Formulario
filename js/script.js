4968eb7039d3490d0e69bc4d8e1c8e9d5964dda6
Valeria Aparecida <valeria777valeria@hotmail.com>
2026-05-19 21:56:28 -0300

    implementa listener

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 function meuEscopo (){
     const form = document.querySelector('.form');
-    form.onsubmit = function(evento){
-        alert(1);
-        console.log("Foi enviado um evento");
-    };
+    const resultado = document.querySelector('.resultado');
+   
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+        
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+        console.log(nome.ariaValueMax, sobrenome, peso, altura);
+
+    }
+    form.addEventListener('submit', recebeEventoForm);
 }
+meuEscopo();
